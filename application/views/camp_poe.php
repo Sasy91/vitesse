ae1d78710e61a709281c9fbdc49b1044ab2b29c0
USER <mydb.mydb2@gmail.com>
2015-06-15 21:13:02 +0530

    camp poe page

--- application/views/camp_poe.php
+++ application/views/camp_poe.php
@@ -27,47 +27,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <!----=====================================================
                     navigation
         =================================================--------->
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Brand</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
-
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">ROOMS<span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Suite 1</a></li>
-                                <li><a href="#">Suite 2</a></li>
-                                <li><a href="#">Suite 3</a></li>
-                                <li><a href="#">Suite 4</a></li>
-                                <li><a href="#">Suite 5</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Studios</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Poolside Bungalows</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">THE CAMP</a></li>
-                        <li><a href="#">TEAM POE</a></li>
-                         <li><a href="#">SHOP</a></li>
-                        <li><a href="#">PHOTOS</a></li>
-                        <li><a href="#">CONTACT</a></li>
-                        <li><a href="#">BOOK NOW</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
+<header>
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#featured"><h1><span class="subhead"></span></h1></a>
+      </div><!-- navbar-header -->
+      <div class="collapse navbar-collapse" id="collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#featured">Home</a></li>
+          <li><a href="#mission">Mission</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#staff">Staff</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+        </ul>        
+      </div><!-- collapse navbar-collapse -->
+    </div><!-- container -->
+  </nav>
+</header>
 
         <!----=====================================================
                   end navigation
