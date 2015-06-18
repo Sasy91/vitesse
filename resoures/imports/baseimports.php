b07eee2d9b9c82f090f88ad68db29d197c3cf279
USER <mydb.mydb2@gmail.com>
2015-06-18 10:32:19 +0530

    navigation

--- resoures/imports/baseimports.php
+++ resoures/imports/baseimports.php
@@ -1,6 +1,7 @@
 
 <link rel="stylesheet" href="<?php echo base_url(); ?>resoures/css/boostrap.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>resoures/css/component.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>resoures/css/smoothness/jquery-ui-1.10.4.custom.min.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>resoures/css/animate.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>resoures/css/media-queries.css">		
