61ddb15e14c5f1ab944f4ae611e1db414f9a983c
Sameera <Sameera@SAMEERAVAIO>
2015-07-09 22:09:41 +0530

    Merge origin/master
    Conflicts:
    	application/controllers/Shop.php

--- resoures/database/camp_poe_1.sql
+++ resoures/database/camp_poe_1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2015 at 12:22 PM
+-- Generation Time: Jul 09, 2015 at 06:38 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `booking` (
-  `b_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) NOT NULL,
   `booked_date` datetime NOT NULL,
   `no_of_guests` varchar(50) NOT NULL,
@@ -30,15 +30,18 @@ CREATE TABLE IF NOT EXISTS `booking` (
   `payment_type` varchar(100) NOT NULL,
   `paid_amount` double(10,2) NOT NULL,
   `audit` varchar(30) NOT NULL,
-  PRIMARY KEY (`b_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`b_id`, `customer_id`, `booked_date`, `no_of_guests`, `childrens`, `adult`, `payment_type`, `paid_amount`, `audit`) VALUES
-(1, 0, '2015-06-21 00:00:00', '4', 2, 2, 'paypal', 25000.00, '');
+INSERT INTO `booking` (`id`, `customer_id`, `booked_date`, `no_of_guests`, `childrens`, `adult`, `payment_type`, `paid_amount`, `audit`) VALUES
+(2, 4, '2015-07-08 09:43:57', '2', 0, 2, 'paypal', 950.00, '::1'),
+(3, 9, '2015-07-09 19:59:21', '1', 0, 1, 'paypal', 550.00, '::1'),
+(4, 9, '2015-07-09 21:33:02', '3', 0, 3, 'paypal', 1125.00, '::1'),
+(1000, 0, '2015-06-21 00:00:00', '4', 2, 2, 'paypal', 25000.00, '');
 
 -- --------------------------------------------------------
 
@@ -53,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `booking_linked` (
   `check_in` datetime NOT NULL,
   `check_out` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `booking_linked`
@@ -61,7 +64,10 @@ CREATE TABLE IF NOT EXISTS `booking_linked` (
 
 INSERT INTO `booking_linked` (`id`, `booking_id`, `room_id`, `check_in`, `check_out`) VALUES
 (1, 1, 2, '2015-07-02 00:00:00', '2015-07-05 00:00:00'),
-(2, 2, 3, '2015-07-01 00:00:00', '2015-07-10 00:00:00');
+(2, 2, 3, '2015-07-01 00:00:00', '2015-07-10 00:00:00'),
+(3, 2, 2, '2015-07-24 00:00:00', '2015-07-30 00:00:00'),
+(4, 3, 2, '2015-07-09 00:00:00', '2015-07-10 00:00:00'),
+(5, 4, 7, '2015-07-28 00:00:00', '2015-07-30 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -229,7 +235,7 @@ INSERT INTO `packagers` (`id`, `name`, `short_detail`, `discription`, `instructo
 (1, 'Surf Level One (Beginners)', '10 hours of surf lessons, free hire of equipment, 2 theory lessons and video analysis.', '5 x 2 hour beginner lessons with an internationally qualified surf instructor and beach lifeguard, free hire of surf equipment, 2 x theory lessons, video analysis.\r\nThis package is perfect if you are completely new to surfing. Over the course of the week, our experienced instructors will break down the basis of surfing to ensure you learn how to stand in a comfortable and safe environment. You will learn how the surfboard works, vital ocean and surfing safety, how to catch waves and tailored techniques to get to your feet. At Camp Poe we believe everyone is capable of catching and surfing on a wave, and we will be there to coach and encourage you all the way!', 'Till Rohrmann', '0', 150.00),
 (2, 'Surf Level Two (Advance)', '10 hours of surf lessons, free hire of equipment, 2 theory lessons and video analysis. ', '5 x 2 hour improver lessons with an internationally qualified surf instructor and beach lifeguard, free hire of surf equipment, 2 x theory lessons, video analysis.\r\nIf you have already had several beginner lessons, and feel comfortable popping up and catching green, or unbroken waves, then this package is a great opportunity for you to take your surfing to the next level. You will have access to Camp Poe’s quiver of surf boards and a two hour session each day with a qualified surf coach who will be offering you invaluable tips as well as taking photos and video footage of your progress. ', 'Till Rohrmann', '0', 150.00),
 (3, 'Surf and Yoga Week', '10 hours of surf lessons, free hire of equipment, 2 theory lessons and video analysis. 6 Yoga sessions.', 'N/A', 'N/A', '0', 190.00),
-(4, 'Yoga Week', '6 yoga sessions.', 'N/A', 'N/A', '0', 50.00);
+(4, 'Yoga Week', '6 yoga sessions.', 'N/A', 'N/A', '0', 40.00);
 
 -- --------------------------------------------------------
 
@@ -241,9 +247,20 @@ CREATE TABLE IF NOT EXISTS `package_linked` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `booking_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
+  `no_of_guest` int(11) NOT NULL,
   `package_id` int(11) NOT NULL,
+  `amount` double(10,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `package_linked`
+--
+
+INSERT INTO `package_linked` (`id`, `booking_id`, `customer_id`, `no_of_guest`, `package_id`, `amount`) VALUES
+(1, 2, 4, 1, 1, 150.00),
+(2, 3, 9, 1, 1, 150.00),
+(3, 4, 9, 3, 1, 450.00);
 
 -- --------------------------------------------------------
 
@@ -265,7 +282,7 @@ CREATE TABLE IF NOT EXISTS `registerd_users` (
   `password` varchar(200) NOT NULL,
   `registerd_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `registerd_users`
@@ -274,7 +291,9 @@ CREATE TABLE IF NOT EXISTS `registerd_users` (
 INSERT INTO `registerd_users` (`id`, `title`, `f_name`, `l_name`, `id_type`, `id_number`, `email`, `country`, `tel`, `user_name`, `password`, `registerd_date`) VALUES
 (3, 'MR.', 'Sasika', 'Ransilu', 'NIC', '9145789671V', 'sameera7@outlook.com', 'SRI LANKA', '94758558596', 'rana0913', '6e9454559ab0f65c702f78d553acab30', '2015-06-26 21:06:20'),
 (4, 'MR.', 'Sasika', 'Ransilu', 'NIC', '9145789671V', 'sameera9th@outlook.com', 'SRI LANKA', '94758558596', 'rana091', '56fafa8964024efa410773781a5f9e93', '2015-06-27 09:41:01'),
-(5, 'MR.', 'Ruchika ', 'Gamage', 'NIC', 'N58985885', 'ruchika.newonline@gmail.com', 'SRI LANKA', '895874585', 'ruchi089', '9f1abb6c236af0b767f84a1adf905127', '2015-06-30 11:44:46');
+(5, 'MR.', 'Ruchika ', 'Gamage', 'NIC', 'N58985885', 'ruchika.newonline@gmail.com', 'SRI LANKA', '895874585', 'ruchi089', '9f1abb6c236af0b767f84a1adf905127', '2015-06-30 11:44:46'),
+(8, 'Not Available', 'Mark', 'N/A', 'Drivers License', 'N47585458', 'sameera@gmail.com', 'Australia', 'N/A', 'mark072', '9880c35d8804ed40f467de976963966b', '2015-07-07 21:14:17'),
+(9, 'MR', 'Til', 'William', 'Passport', 'N47585458', 'sameera@vitessesoft.com', 'Germany', '78548557854', 'til', '645dd46adc34f3eb91dd2751469abd9e', '2015-07-09 19:56:52');
 
 -- --------------------------------------------------------
 
@@ -407,7 +426,7 @@ CREATE TABLE IF NOT EXISTS `temp_booking_data` (
   `check_out` datetime NOT NULL,
   `audit` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=80 ;
 
 --
 -- Dumping data for table `temp_booking_data`
@@ -420,7 +439,23 @@ INSERT INTO `temp_booking_data` (`id`, `unique_id`, `room_id`, `tot_guest`, `adu
 (46, '5598c4a7c7fdf', 1, 1, 1, 0, 1650.00, 'SUITE 1', 0.00, '2015-07-13 00:00:00', '2015-07-30 00:00:00', '::1'),
 (47, '5598e919d84ba', 2, 2, 2, 0, 950.00, 'SUITE 2', 550.00, '2015-07-27 00:00:00', '2015-07-30 00:00:00', '::1'),
 (49, '5598eb78c9b56', 1, 2, 2, 0, 950.00, 'SUITE 1', 550.00, '2015-07-05 00:00:00', '2015-07-06 00:00:00', '::1'),
-(50, '5598eb78c9b56', 7, 3, 2, 1, 937.50, 'FLEXIBLE FAMILY SUITE', 375.00, '2015-07-05 00:00:00', '2015-07-06 00:00:00', '::1');
+(50, '5598eb78c9b56', 7, 3, 2, 1, 937.50, 'FLEXIBLE FAMILY SUITE', 375.00, '2015-07-05 00:00:00', '2015-07-06 00:00:00', '::1'),
+(51, '5599083e77c13', 8, 4, 2, 2, 9450.00, 'BUNK SUITE', 350.00, '2015-05-07 00:00:00', '2015-07-07 00:00:00', '::1'),
+(53, '55991befce249', 1, 1, 1, 0, 1100.00, 'SUITE 1', 550.00, '2015-07-11 00:00:00', '2015-07-23 00:00:00', '::1'),
+(54, '55991befce249', 4, 1, 1, 0, 550.00, 'SUITE 4', 550.00, '2015-07-05 00:00:00', '2015-07-06 00:00:00', '::1'),
+(56, '55991befce249', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-05 00:00:00', '2015-07-06 00:00:00', '::1'),
+(57, '55991befce249', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-05 00:00:00', '2015-07-06 00:00:00', '::1'),
+(58, '55994c93523d9', 2, 1, 1, 0, 550.00, 'SUITE 2', 550.00, '2015-07-06 00:00:00', '2015-07-07 00:00:00', '::1'),
+(59, '55994c93523d9', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-06 00:00:00', '2015-07-07 00:00:00', '::1'),
+(63, '559a176367b45', 1, 2, 2, 0, 950.00, 'SUITE 1', 550.00, '2015-07-20 00:00:00', '2015-07-22 00:00:00', '::1'),
+(64, '559a176367b45', 3, 2, 2, 0, 950.00, 'SUITE 3', 550.00, '2015-07-19 00:00:00', '2015-07-24 00:00:00', '::1'),
+(65, '559a93cd386fd', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-06 00:00:00', '2015-07-07 00:00:00', '::1'),
+(66, '559aa232e70d7', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-06 00:00:00', '2015-07-07 00:00:00', '::1'),
+(68, '559bdbda1cf36', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-07 00:00:00', '2015-07-08 00:00:00', '::1'),
+(69, '559bdc3ec9c2e', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-07 00:00:00', '2015-07-08 00:00:00', '::1'),
+(73, '559be3759b82f', 1, 2, 2, 0, 950.00, 'SUITE 1', 550.00, '2015-07-07 00:00:00', '2015-07-08 00:00:00', '::1'),
+(75, '559ca3017317b', 2, 2, 2, 0, 950.00, 'SUITE 2', 550.00, '2015-07-24 00:00:00', '2015-07-30 00:00:00', '::1'),
+(78, '559e8784f1b0c', 1, 1, 1, 0, 550.00, 'SUITE 1', 550.00, '2015-07-09 00:00:00', '2015-07-10 00:00:00', '::1');
 
 -- --------------------------------------------------------
 
@@ -437,7 +472,7 @@ CREATE TABLE IF NOT EXISTS `temp_package_data` (
   `no_of_guests` varchar(50) NOT NULL,
   `amount` double(10,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=50 ;
 
 --
 -- Dumping data for table `temp_package_data`
@@ -449,7 +484,21 @@ INSERT INTO `temp_package_data` (`id`, `unique_id`, `package_name`, `package_amo
 (26, '5598c4a7c7fdf', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
 (27, '5598e919d84ba', 'Surf Level One (Beginners)', 150.00, '1', '2', 300.00),
 (28, '5598eb78c9b56', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
-(29, '5598eb78c9b56', 'Surf Level Two (Advance)', 150.00, '2', '2', 300.00);
+(29, '5598eb78c9b56', 'Surf Level Two (Advance)', 150.00, '2', '2', 300.00),
+(30, '5599083e77c13', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(31, '5599083e77c13', 'Surf Level Two (Advance)', 150.00, '2', '1', 150.00),
+(32, '559909a23468d', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(33, '559909a23468d', 'Surf Level Two (Advance)', 150.00, '2', '1', 150.00),
+(34, '55991befce249', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(35, '55991befce249', 'Surf Level Two (Advance)', 150.00, '2', '1', 150.00),
+(36, '559a105557bd5', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(37, '559a176367b45', 'Surf Level One (Beginners)', 150.00, '1', '2', 300.00),
+(38, '559a93cd386fd', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(39, '559aa232e70d7', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(40, '559bdbda1cf36', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(41, '559bdc3ec9c2e', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(45, '559be3759b82f', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00),
+(46, '559ca3017317b', 'Surf Level One (Beginners)', 150.00, '1', '1', 150.00);
 
 -- --------------------------------------------------------
 
@@ -475,4 +524,4 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `user_id`, `password`, `role`, `last_login`, `registerd_date`, `audit`, `name`, `img_url`) VALUES
-(1, 'sameera@vitessesoft.com', '56fafa8964024efa410773781a5f9e93', 'admin', '2015-07-01 06:08:58', '2015-06-12 00:00:00', '::1', 'Mark William', '');
+(1, 'sameera@vitessesoft.com', '56fafa8964024efa410773781a5f9e93', 'admin', '2015-07-07 21:03:37', '2015-06-12 00:00:00', '::1', 'Mark William', '');
